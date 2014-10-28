2f26dfc6bc1a8f1eeb4c254b8180f8f4ea45224e
mcclaskiem <max.mcclaskie@gmail.com>
2014-10-28 09:49:20 -0400

    added a node server to the project

---
+++ server.js
@@ -0,0 +1,14 @@
+var fs = require("fs");
+var host = "0.0.0.0";
+var port = 8000;
+var express = require("express");
+
+var app = express(); //use both root and other routes below
+app.use(express.static(__dirname + "/app")); //use static files in ROOT/public folder
+
+app.get("/", function(request, response){ //root dir
+    response.send("Hello!!");
+});
+
+app.listen(port, host);
+
