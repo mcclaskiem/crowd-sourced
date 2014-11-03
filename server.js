729e6b9cf3f5477fdafbe7260e85446b47a85082
Scott Kuchinski <tuf13957@temple.edu>
2014-11-03 11:39:55 -0500

    Update server.js
    Mongoose connect auth update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-	mongoose.connect('mongodb://maxmc:dukelax01@ds049160.mongolab.com:49160/polymer-music');
+	mongoose.connect('mongodb://admin:admmin1234@ds047950.mongolab.com:47950/polymer_music');
 var Post = require('./api/posts');
 
 	app.use(bodyParser.urlencoded({ extended: true }));
